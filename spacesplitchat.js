96772da4e37cababc4e641f0189a3172475dc2d3
yimmyj <69327109+yimmyj@users.noreply.github.com>
2021-02-02 23:28:24 -0800

    Create spacesplitchat.js

---
+++ spacesplitchat.js
@@ -0,0 +1,262 @@
+/*
+var songString;
+let spaceIndex = 0;
+counterBox = 0;
+setInterval(() => {
+    if(messageToggle == 0) {
+
+        let songString = `More than just a dream More than just a dream Forty days and forty nights
+ I waited for a girl like you to come and save my life
+ All the days I waited for you
+ You know the ones who said I'd never find someone like you
+ And you were out of my league
+ All the things I believed
+ You were just the right kind
+ Yeah, you were more than just a dream
+ You were out of my league
+ Got my heartbeat racing
+ If I die, don't wake me
+ 'Cause you are more than just a dream
+ From time to time I pinch myself
+ Because I think my girl mistakes me for somebody else
+ And every time she takes my hand
+ All the wonders that remain
+ Become a simple fact
+ That you were out of my league
+ All the things I believed
+ You were just the right kind
+ Yeah, you were more than just a dream
+ You were out of my league
+I was scared of dentists and the dark
+I was scared of pretty girls and starting conversations
+Oh, all my friends are turning green
+You're the magician's assistant in their dream
+Ah-ooh-ooh-ooh, ah-ahh-ah-ah
+And they come unstuck
+Lady, running down to the riptide
+Taken away to the dark side
+I wanna be your left hand man
+I love you when you're singing that song
+And I got a lump in my throat
+'Cause you're gonna sing the words wrong
+There's this movie that I think you'll like
+This guy decides to quit his job and heads to New York City
+This cowboy's running from himself
+And she's been living on the highest shelf
+Ah-ooh-ooh-ooh, ah-ahh-oh
+And they come unstuck
+Lady, running down to the riptide
+Taken away to the dark side
+I wanna be your left hand man
+I love you when you're singing that song
+And I got a lump in my throat
+'Cause you're gonna sing the words wrong
+I just wanna, I just wanna know
+If you're gonna, if you're gonna stay
+I just gotta, I just gotta know
+I can't have it, I can't have it any other way
+I swear she's destined for the screen
+Closest thing to Michelle Pfeiffer that you've ever seen, oh
+Lady, running down to the riptide
+Taken away to the dark side
+I wanna be your left hand man
+I love you when you're singing that song
+And I got a lump in my throat
+'Cause you're gonna sing the words wrong
+Oh lady, running down to the riptide
+Taken away to the dark side
+I wanna be your left hand man
+I love you when you're singing that song
+And I got a lump in my throat
+'Cause you're gonna sing the words wrong
+Oh lady, running down to the riptide
+Taken away to the dark side
+I wanna be your left hand man
+I love you when you're singing that song
+And I got a lump in my throat
+'Cause you're gonna sing the words wrong
+Yeah, I got a lump in my throat
+'Cause you're gonna sing the words wrong
+Astro, yeah
+Sun is down, freezin' cold
+That's how we already know winter's here
+My dawg would prolly do it for a Louis belt
+That's just all he know, he don't know nothin' else
+I tried to show 'em, yeah
+I tried to show 'em, yeah, yeah
+Yeah, yeah, yeah
+Gone on you with the pick and roll
+Young LaFlame, he in sicko mode
+Woo, made this here with all the ice on in the booth
+At the gate outside, when they pull up, they get me loose
+Yeah, Jump Out boys, that's Nike boys, hoppin' out coupes
+This shit way too big, when we pull up give me the loot
+(Gimme the loot!)
+Was off the Remy, had a Papoose
+Had to hit my old town to duck the news
+Two-four hour lockdown, we made no moves
+Now it's 4AM and I'm back up poppin' with the crew
+I just landed in, Chase B mixes pop like Jamba Juice
+Different colored chains, think my jeweler really sellin' fruits
+And they chokin', man, know the crackers wish it was a noose
+Some-some-some, someone said
+To win the retreat, we all in too deep
+P-p-playin' for keeps, don't play us for weak (someone said)
+To win the retreat, we all in too deep
+P-p-playin' for keeps, don't play us for weak (yeah)
+This shit way too formal, y'all know I don't follow suit
+Stacey Dash, most of these girls ain't got a clue
+All of these hoes I made off records I produced
+I might take all my exes and put 'em all in a group
+Hit my esés, I need the bootch
+'Bout to turn this function to Bonnaroo
+Told her, "Hop in, you comin' too"
+In the 305, bitches treat me like I'm Uncle Luke
+(Don't stop, pop that pussy!)
+Had to slop the top off, it's just a roof (uh)
+She said, "Where we goin'?" I said, "The moon"
+We ain't even make it to the room
+She thought it was the ocean, it's just the pool
+Now I got her open, it's just the Goose
+Who put this shit together? I'm the glue (someone said)
+Shorty FaceTimed me out the blue
+Someone said (playin' for keeps)
+Someone said, motherfuck what someone said
+(Don't play us for weak)
+Yeah
+Astro
+Yeah, yeah
+Tay Keith, fuck these niggas up (Ay, ay)
+She's in love with who I am
+Back in high school, I used to bus it to the dance (yeah)
+Now I hit the FBO with duffles in my hands
+I did half a Xan, thirteen hours 'til I land
+Had me out like a light, ayy, yeah
+Like a light, ayy, yeah
+Like a light, ayy
+Slept through the flight, ayy
+Knocked for the night, ayy, 767, man
+This shit got double bedroom, man
+I still got scores to settle, man
+I crept down the block (down the block), made a right (yeah, right)
+Cut the lights (yeah, what?), paid the price (yeah)
+Niggas think it's sweet (nah, nah), it's on sight (yeah, what?)
+Nothin' nice (yeah), baguettes in my ice (aww, man)
+Jesus Christ (yeah), checks over stripes (yeah)
+That's what I like (yeah), that's what we like (yeah)
+Lost my respect, you not a threat
+When I shoot my shot, that shit wetty like I'm Sheck (bitch!)
+See the shots that I took (ayy), wet like I'm Book (ayy)
+Wet like I'm Lizzie, I be spinnin' Valley
+Circle blocks 'til I'm dizzy (yeah, what?)
+Like where is he? (Yeah, what?)
+No one seen him (yeah, yeah)
+I'm tryna clean 'em (yeah)
+She's in love with who I am
+Back in high school, I used to bus it to the dance
+Now I hit the FBO with duffles in my hand (woo!)
+I did half a Xan, thirteen hours 'til I land
+Had me out like a light, like a light
+Like a light, like a light
+Like a light (yeah), like a light
+Like a light
+Yeah, passed the dawgs a celly
+Sendin' texts, ain't sendin' kites, yeah
+He said, "Keep that on lock"
+I said, "You know this shit, it's stife", yeah
+It's absolute (yeah), I'm back reboot (it's lit!)
+LaFerrari to Jamba Juice, yeah (skrrt, skrrt)
+We back on the road, they jumpin' off, no parachute, yeah
+Shawty in the back
+She said she workin' on her glutes, yeah (oh my God)
+Ain't by the book, yeah
+This how it look, yeah
+'Bout a check, yeah
+Just check the foots, yeah
+Pass this to my daughter, I'ma show her what it took (yeah)
+Baby mama cover Forbes, got these other bitches shook, yeah
+Ye-ah
+These bitches love Sosa
+And they love them Glo' Boys
+Know we from the 'Go boy
+But we cannot go boy
+No I don't know ol' boy
+I know he's a broke boy
+Raris and Rovers
+Convertible Lambos boy
+You know I got bands boy
+And it's in my pants boy
+Disrespect them O Boys
+You won't speak again boy
+Don't think that I'm playin boy
+No we don't use hands boy
+No we don't do friends boy
+Collect bands I'm a land lord
+I gets lotsa commas
+I can fuck yo mama
+I ain't with the drama
+You can meet my llama
+Ridin with 3hunna
+With 300 foreigns
+These bitches see Chief Sosa
+I swear to god they all honored
+These bitches love Sosa
+O End or no end
+Fucking with them O boys
+You gon' get fucked over
+Raris and Rovers
+These hoes love Chief Sosa
+Hit him with that cobra
+Now that boy slumped over
+They do it all for Sosa
+You boys ain't making no noise
+Y'all know I'm a grown boy
+Your clique full of broke boys
+God ya'll some broke boys
+God ya'll some broke boys
+We GBE dope boys
+We got lots of dough boy
+Don't make me call D. Rose boy
+He six double-o boy
+And he keep that pole boy
+You gon' get fucked over
+Bitch I done sell soda
+And I done sell coca
+She gon' clap for Sosa
+He gon' clap for Sosa
+They do it for Sosa
+Them hoes they do it for Sosa
+Tadoe off the Molly water
+So nigga be cool like water
+Fore you get hit with this lava
+Bitch I'm the trending topic
+Don't care no price I'll cop it b
+And yo bitch steady jockin' me
+These bitches love Sosa
+O End or no end
+Fucking with them O boys
+You gon' get fucked over
+Raris and Rovers
+These hoes love Chief Sosa
+Hit him with that cobra
+Now that boy slumped over
+They do it all for Sosa
+You boys ain't making no noise
+Y'all know I'm a grown boy
+Your clique full of broke boys
+God ya'll some broke boys
+God ya'll some broke boys
+We GBE dope boys
+We got lots of dough boy`;
+        if (counterBox > songString.length){
+            counterBox = 0;
+        }
+        spaceIndex = counterBox + 30;
+        while (songString.charAt(spaceIndex) != " "){
+            spaceIndex --;
+        }
+        doNewSend(["ch", [songString.substring(counterBox, spaceIndex)]])
+    }
+counterBox = spaceIndex;
+}, 2300);
+*/
